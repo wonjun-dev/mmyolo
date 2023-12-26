7e7234a5c277e4891d1a79e07bd736cc914beb18
Wonjun <50730434+wonjun-dev@users.noreply.github.com>
2023-12-26 09:38:24 +0900

    Update __init__.py

--- mmyolo/__init__.py
+++ mmyolo/__init__.py
@@ -20,18 +20,18 @@ mmdet_version = digit_version(mmdet.__version__)
 
 
 assert (mmcv_version >= digit_version(mmcv_minimum_version)
-        and mmcv_version < digit_version(mmcv_maximum_version)), \
+        and mmcv_version <= digit_version(mmcv_maximum_version)), \
     f'MMCV=={mmcv.__version__} is used but incompatible. ' \
     f'Please install mmcv>={mmcv_minimum_version}, <{mmcv_maximum_version}.'
 
 assert (mmengine_version >= digit_version(mmengine_minimum_version)
-        and mmengine_version < digit_version(mmengine_maximum_version)), \
+        and mmengine_version <= digit_version(mmengine_maximum_version)), \
     f'MMEngine=={mmengine.__version__} is used but incompatible. ' \
     f'Please install mmengine>={mmengine_minimum_version}, ' \
     f'<{mmengine_maximum_version}.'
 
 assert (mmdet_version >= digit_version(mmdet_minimum_version)
-        and mmdet_version < digit_version(mmdet_maximum_version)), \
+        and mmdet_version <= digit_version(mmdet_maximum_version)), \
     f'MMDetection=={mmdet.__version__} is used but incompatible. ' \
     f'Please install mmdet>={mmdet_minimum_version}, ' \
     f'<{mmdet_maximum_version}.'
